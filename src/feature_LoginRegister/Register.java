828e8a01768ccfdb4ff0fa1c53b463928d5bbb94
kraftrichard <kraftrichard@102WKS06.bsz.edu>
2019-03-28 10:09:31 +0100

    Konstruktor und Attribute hinzugefügt

--- src/feature_LoginRegister/Register.java
+++ src/feature_LoginRegister/Register.java
@@ -2,5 +2,45 @@ package feature_LoginRegister;
 
 public class Register
 {
+	int protocolCase;
+	String username;
+	String password;
+	
+	public Register(int protCase, String uname, String pass)
+	{
+		setProtocolCase(protCase);
+		setUsername(uname);
+		setPassword(pass);
+	}
+
+	public int getProtocolCase()
+	{
+		return protocolCase;
+	}
+
+	public void setProtocolCase(int protocolCase)
+	{
+		this.protocolCase = protocolCase;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
 
 }
