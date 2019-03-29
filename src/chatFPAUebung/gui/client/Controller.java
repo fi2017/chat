384c4c0cf8397bed693b57e14a2b8daadf980808
MichaelWinsauer <46480126+MichaelWinsauer@users.noreply.github.com>
2019-03-29 09:10:27 +0100

    CloseButton eingebaut für Test mit Marcel

--- src/chatFPAUebung/gui/client/Controller.java
+++ src/chatFPAUebung/gui/client/Controller.java
@@ -1,6 +1,22 @@
 package chatFPAUebung.gui.client;
 
-public class Controller
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
 {
 
+    public Button btnClose;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        btnClose.setOnAction(e -> {
+            ((Stage)btnClose.getScene().getWindow()).close();
+        });
+    }
 }
