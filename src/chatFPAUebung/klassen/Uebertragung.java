5a94f71b05e4d93acf56bd76a78cca2fa181e148
HanenbergJoshua <HanenbergJoshua@102WKS09.bsz.edu>
2019-03-29 11:47:39 +0100

    Security Update
    Removed that the client decides when the message was transmitted and
    calculated it server-side

--- src/chatFPAUebung/klassen/Uebertragung.java
+++ src/chatFPAUebung/klassen/Uebertragung.java
@@ -19,6 +19,13 @@ public class Uebertragung implements Serializable
 		this.uebertragungszeitpunkt = LocalDateTime.now();
 	}
 
+	public Uebertragung(Uebertragung uebertragungCopy)
+	{
+		this.zweck = uebertragungCopy.getZweck();
+		this.uebertragung = uebertragungCopy.uebertragung;
+		this.uebertragungszeitpunkt = LocalDateTime.now();
+	}
+
 	// Getter
 	public int getZweck()
 	{
