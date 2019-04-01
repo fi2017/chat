d37323ed5c62544be45048de0749c157dcd824c3
Blendan <37484137+Blendan@users.noreply.github.com>
2019-04-01 08:52:36 +0200

    added interfaces for remote admintool

---
+++ src/chatFPAUebung/interfaces/BlackListInterface.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+/*
+	Autor: Philipp Hägerich
+
+	This work ist made for the Chat-Project from the FI2017 from the BSZ
+ */
+
+public interface BlackListInterface
+{
+	void addUserToBlackList(String username);
+	void removeUserFromBlackList(String username);
+	boolean canUserJoint(String username);
+}
